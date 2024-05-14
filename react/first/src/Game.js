4ac705e633fec046504195516bdf31855e3b2da8
Lee JunHo <jhh12@naver.com>
2024-05-14 17:29:22 +0900

    2024-05-14 17:29

--- react/first/src/Game.js
+++ react/first/src/Game.js
@@ -1,26 +1,27 @@
-import React from "react"
-import "./Game.css"
+import React from "react";
+import "./Game.css";
 
 class Square extends React.Component {
-    // constructor(props) {
-    //     super(props);
-    //     console.log(props);
-    //     this.state = {  // state 초기화
-    //         value: null,
-    //     }
-    // }
+    constructor(props) {
+        super(props);
+
+        // this.state = {
+        //     value: null
+        // };
+    }
+
     render() {
-        // console.log('render');
-        return (
-            <button className="square" onClick={this.props.onClick}>
-                {this.props.value}
-            </button>
+            // console.clear();
+            return (
+            <button className="square" 
+            onClick={this.props.onClick}
+            >{this.props.value}</button>
         );
     }
 }
 
 function calculateWinner(squares) {
-    const lines = [
+    const lines = [ // 이길 수 있는 경우의 수
         [0, 1, 2],
         [3, 4, 5],
         [6, 7, 8],
@@ -33,91 +34,133 @@ function calculateWinner(squares) {
 
     for (let i = 0; i < lines.length; i++) {
         const [a, b, c] = lines[i];
-        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        // console.log(squares[a], squares[b], squares[c]);
+        if (squares[a] && squares[a] === squares[b] && squares[a] && squares[c]) {
             return squares[a];
-        } 
+        }
     }
+
     return null;
 }
 
+
 class Board extends React.Component {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            squares: Array(9).fill(null),
-            xIsNext: true
-        };
-
-        // console.log(this.state.squares);
-    }
-
-    handleClick(i) {
-        if (calculateWinner(this.state.squares) || this.state.squares[i]) { // 누군가가 이겼으면 게임 종료
-            return;
-        }
-        // 불변성
-        const squares = this.state.squares.slice();
-        squares[i] = this.state.xIsNext ? 'X' : 'O';
-        this.setState({
-            squares: squares,
-            xIsNext: !this.state.xIsNext
-        }); // 비동기 처리
-        // console.log(this.state.squares); // 처리
-    }
-
-    renderSqure(i) {
-        return <Square 
-                    value={this.state.squares[i]}
-                    onClick={() => this.handleClick(i)}/>
+    renderSquare(i) {
+        return (
+            <Square value={this.props.squares[i]} 
+            onClick={() => this.props.onClick(i)} />
+        );
     }
 
     render() {
-        const winner = calculateWinner(this.state.squares);
-        let status;
-        // console.log(winner);
-        if (winner) {
-            status = `Winner: ${winner}`
-        } else {
-            status = `Next Player: ${this.state.xIsNext ? 'X' : 'O'}`;
-        }
-        
         return (
             <div>
-                <div className="status">{status}</div>
                 <div className="board-row">
-                    {this.renderSqure(0)}
-                    {this.renderSqure(1)}
-                    {this.renderSqure(2)}
+                    {this.renderSquare(0)}
+                    {this.renderSquare(1)}
+                    {this.renderSquare(2)}
                 </div>
                 <div className="board-row">
-                    {this.renderSqure(3)}
-                    {this.renderSqure(4)}
-                    {this.renderSqure(5)}
+                    {this.renderSquare(3)}
+                    {this.renderSquare(4)}
+                    {this.renderSquare(5)}
                 </div>
                 <div className="board-row">
-                    {this.renderSqure(6)}
-                    {this.renderSqure(7)}
-                    {this.renderSqure(8)}
+                    {this.renderSquare(6)}
+                    {this.renderSquare(7)}
+                    {this.renderSquare(8)}
                 </div>
             </div>
         );
     }
 }
 
-class Game extends React.Component {
+class Game extends React.Component { // Object
+    constructor(props) {
+        super(props);
+
+        let lsGame = JSON.parse(localStorage.getItem("lsGame"));
+
+        if (!lsGame) {
+            lsGame = {
+                history: [
+                    {
+                        squares: Array(9).fill(null)
+                    }
+                ],
+                xIsNext: true,
+                stepNumber: 0
+            };
+        }
+
+        this.state = lsGame;
+    }
+
+    handleClick(i) {
+        // 불변성
+        let history = this.state.history.slice(0, this.state.stepNumber + 1);
+        const xIsNext = this.state.xIsNext;
+        const squares = history[history.length - 1].squares.slice();
+        
+        if (squares[i]) return;
+
+        if (calculateWinner(squares)) return;
+        
+        console.log(squares[i]);
+
+        squares[i] = xIsNext ? 'X' : 'O';
+        history = history.concat({squares:squares});
+        this.setState({history: history, xIsNext: !xIsNext, stepNumber: this.state.stepNumber + 1});
+
+        localStorage.setItem("lsGame", JSON.stringify(this.state));
+        console.log(localStorage.getItem("lsGame"));
+    }
+
+    jumpTo(move) {
+        console.log(move);
+        this.setState({
+            stepNumber: move,
+             xIsNext: move % 2 === 0
+        });
+    }
+
     render() {
+        // console.clear();
+        const history = this.state.history.slice();
+        const current = history[this.state.stepNumber].squares;
+        // console.log(current);
+        
+        let status;
+        let winner = calculateWinner(current);
+        if (winner) {
+            status = 'Winner: ' + winner;
+        } else {
+            status = 'Next Player: ' + (this.state.xIsNext ? 'X' : 'O');
+        }
+
+        // 배열 map => 특정한 작업 후 새로운 배열을 리턴
+        
+        const moves = history.map((step, move) => {
+            const desc = move ? 'Go to move #' + move : 'Go to start'
+            return (
+                <li key={move}>
+                    <button onClick={() => this.jumpTo(move)}>{desc}</button>
+                </li>
+            );
+        });
+
         return (
             <div className="game">
                 <div className="game-board">
-                    <Board />
+                    <Board squares={current} onClick={(i) => this.handleClick(i)} />
                 </div>
                 <div className="game-info">
-                    <div></div>
-                    <ol></ol>
+                    <div>{status}</div>
+                    <ol>{moves}</ol>
                 </div>
             </div>
-        )
+        );
     }
 }
 
--- todolist/todolist.js
+++ todolist/todolist.js
@@ -1,160 +1,104 @@
+let todoList = document.getElementById("todoList"); // 리스트를 추가할 ul
+
 // 오늘 날짜 보이기
-let todoList = document.getElementById("todoList"); // ul
-// const toDoForm = document.querySelector("#form");
-// let toDoInput = toDoForm.querySelector("input");
 let day = new Date();
 let options = {year: "numeric", month: "long", day: "numeric", weekday: "long"};
 let today = day.toLocaleDateString("kr-KR", options);
-
 showDate.innerHTML = today;
-showDate.style.fontSize = "24px";
-showDate.style.padding = "10px";
-// showDate.style.background = "#6190E8";  /* fallback for old browsers */
-// showDate.style.background = "-webkit-linear-gradient(to right, #A7BFE8, #6190E8)";  /* Chrome 10-25, Safari 5.1-6 */
-// showDate.style.background = "linear-gradient(to right, #A7BFE8, #6190E8)"; /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
-// showDate.style.color = "#fff";
-// showDate.style.width = "40%";
-showDate.style.margin = "10px auto";
-// showDate.style.borderRadius = "50%";
-showDate.style.fontWeight = "bold";
 
+// 플래그
 let flag = 1;
 
-let undoList = 0; // 남은 일
-let doList = 0;   // 한 일
+// 완료한 일 카운트
+let comp = 0;
+leftTodo.innerHTML = `남은 일: ${todoList.children.length}개 / 완료한 일: ${comp}개`;
 
-leftTodo.innerHTML = `남은 일: ${undoList}개, 한 일: ${doList}개`;
-
-
-// const ToDo_LocalStroage = "toDos";
-// let toDos = [];
-
-
-// let saveToDos = () => {
-//     localStorage.setItem(ToDo_LocalStroage, JSON.stringify(toDos));
-// }
-
-// let loadToDos = () => {
-//     const loadedToDos = localStorage.getItem(ToDo_LocalStroage);
-
-//     if (loadedToDos !== null) {
-//         const parsedToDos = JSON.parse(loadedToDos);
-//         parsedToDos.forEach((toDo) => {
-//             addList(toDo.text);
-//         })
-//     }
-// }
-
-// handleSubmit = (event) => {
-//     event.preventDefault();
-//     const currentValue = toDoInput.value;
-//     (currentValue === ''? emptyToDo() : existToDo(currentValue))
-// }
-
-// let init = () => {
-//     loadToDos();
-//     toDoForm.addEventListener("submit", handleSubmit);
-    
-// }
+// 완료한 일 계산 함수
+function compCal(n) {
+    return comp += n;
+}
 
 // 리스트 추가 함수
 function addList() {
     let checkBtn = document.createElement("button");    // 체크 버튼
-    
     checkBtn.className = "checkBtn";  
-    checkBtn.style.background = "#fff";
-    checkBtn.style.width = "25px";
-    checkBtn.style.height = "25px";
-    checkBtn.style.marginRight = "10px";
-    checkBtn.style.marginTop = "2px";
-    checkBtn.style.float = "left";
-    checkBtn.style.border = "1px solid #aaa";
-    checkBtn.style.color = "black";
-    
     checkBtn.addEventListener("click", checkList);  // 체크 버튼이 클릭되면 checkList 함수 작동
     
-    let li = document.createElement("li");
+    let li = document.createElement("li");  // 리스트를 저장할 li
+
     let deleteBtn = document.createElement("button");  // 삭제 버튼
     let todoValue = document.getElementById("todo").value;  // input의 입력 값
 
     li.append(todoValue);
     li.append(checkBtn);
     li.append(deleteBtn);
-
-    deleteBtn.innerHTML = "삭제";
-    deleteBtn.style.float = "right";
-    deleteBtn.style.padding = "10px";
+    
+    deleteBtn.setAttribute("class", "trash deleteBtn fa-solid fa-trash-can");
     deleteBtn.addEventListener("click", delOneList);  // 삭제버튼 클릭시 리스트 지우기
     
     if (todoValue === "") { // 입력창이 비어있다면
         alert("할 일을 입력해주세요");  // 경고창 띄욱
     } else {
         todoList.appendChild(li);
-        undoList += 1;
-        leftTodo.innerHTML = `남은 일: ${undoList}개, 한 일: ${doList}개`;
-        // toDos.push(todoValue);
-        // saveToDos();
-    }
+        document.getElementById("todo").value = "";
 
+        leftTodo.innerHTML = `남은 일: ${todoList.children.length}개 / 완료한 일: ${comp}개`;
+    }
+    todo.focus();
 }
 
-// 엔터로 리스트 추가되게 만들기
+// 엔터로 리스트 추가 가능하게
 function EnterList(event) {
     if (event.keyCode == "13") {
         addList();
-        document.getElementById("todo").value = "";
     }
 }
 
-// 완료한 일 체크
+
+// 완료한 일 체크 함수
 function checkList(e) {
     let check = e.target.parentElement; // li
     let chkBtn = e.target;  // 체크 버튼
-    
+    // let left = todoList.children.length - comp;
     flag = 0;
 
     if (flag == 1 || chkBtn.innerHTML == "") {
         check.style.backgroundColor = "#bbb";
         check.style.textDecoration = "line-through";
         check.style.opacity = "0.5";
-        chkBtn.innerText = "✔";
+        chkBtn.innerHTML = "✔";
         chkBtn.style.lineHeight = "5px";
-        console.log(`전 flag: ${flag}`);
         flag = 0;
-        console.log(`후 flag: ${flag}, 체크`);
-        doList += 1;
-        leftTodo.innerHTML = `남은 일: ${undoList}개, 한 일: ${doList}개`;
+        
+        compCal(1); // comp + 1
+        
+        leftTodo.innerHTML = `남은 일: ${todoList.children.length}개 / 완료한 일: ${comp}개`;
+        // console.log(`comp: ${comp}, left: ${left}`);
+        
     } else {
         check.style.backgroundColor = "#eee";
         check.style.textDecoration = "none";
         check.style.opacity = "1";
         chkBtn.innerHTML = "";
-        console.log(`전 flag: ${flag}`);
         flag = 1;
-        console.log(`flag: ${flag}, 체크 취소`);
-        doList -= 1;
-        leftTodo.innerHTML = `남은 일: ${undoList}개, 한 일: ${doList}개`;
+
+        compCal(-1); // comp - 1
+
+        leftTodo.innerHTML = `남은 일: ${todoList.children.length}개 / 완료한 일: ${comp}개`;
+        // console.log(`comp: ${comp}, left: ${left}`);
     }
-    
-    console.log(flag);
 }
 
 // 리스트 삭제 함수
 function delOneList(e) {
-    // let delOne = e.target.parentElement;
-    // delOne.remove();
     let li = e.target.parentNode;
     todoList.removeChild(li);
 
-
-
-    undoList -= 1;
+    leftTodo.innerHTML = `남은 일: ${todoList.children.length}개 / 완료한 일: ${comp}개`
     
-    leftTodo.innerHTML = `남은 일: ${undoList}개, 한 일: ${doList}개`;
 }
 
-
-// 리스트 모두 삭제
+// 리스트 모두 삭제 함수
 function delAllList() {
     let yesDel = confirm("정말로 모든 리스트를 삭제하시겠습니까?", "");
 
@@ -162,8 +106,7 @@ function delAllList() {
         alert("삭제할 리스트가 없습니다.");
     } else if (yesDel) {
         todoList.innerHTML = "";  // 확인을 클릭하면 리스트 비우기
-        undoList = 0;
-        leftTodo.innerHTML = `남은 일: ${undoList}개, 한 일: ${doList}개`;
+        leftTodo.innerHTML = `남은 일: ${todoList.children.length - todoList.children.length}개, 완료한 일: ${comp}개`
     }
 }
 
