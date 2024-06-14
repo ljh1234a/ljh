85a8336137882f554e01de8b05bf289bea826694
Lee JunHo <jhh12@naver.com>
2024-06-14 17:28:57 +0900

    '2024-06-14'

--- node/chat/chat.js
+++ node/chat/chat.js
@@ -1,6 +1,7 @@
 const fs = require("fs");
 const http = require("http");
 const socket = require("socket.io");
+const mysql = require("mysql");
 const port = 9999;
 
 const server = http
@@ -14,51 +15,73 @@ const server = http
     console.log(port);
   });
 
+const client = mysql.createConnection({
+  user: "root",
+  password: "0000",
+  database: "aws",
+});
+
+client.connect();
+
 const io = socket.listen(server);
 
 let users = {};
 
 // connection 이벤트, disconnection 이벤트가 발생
 io.sockets.on("connection", function (so) {
-  console.dir(so);
+  // console.dir(so);
+
+  client.query("SELECT * FROM chat", (err, result) => {
+    if (err) {
+      console.log(err.sqlMessage);
+    } else {
+      result.forEach((chat) => {
+        const data = {
+          uname: chat.uname,
+          message: chat.message,
+          ucolor: chat.ucolor,
+          time: chat.time,
+        };
+        so.emit("showMsg", data);
+      });
+    }
+  });
 
   so.on("In", function (data) {
     users[so.id] = data.uname;
+
     io.sockets.emit("userIn", { uname: data.uname });
     io.sockets.emit("updateUsers", Object.values(users));
   });
 
   so.on("msg", function (data) {
+    data.sid = so.id;
+
+    const sql =
+      "INSERT INTO chat (uname, message, ucolor, time) VALUES (?, ?, ?, ?)";
+    const params = [data.uname, data.message, data.ucolor, data.time];
+    client.query(sql, params, (err) => {
+      if (err) {
+        console.log(err.sqlMessage);
+      }
+    });
+
+    // public 모든 클라이언트에게 전송
     io.sockets.emit("showMsg", data);
+
+    // broadcast 자신을 제외한 클라이언트들에게 전송
+    // so.broadcast.emit("showMsg", data);
+
+    // private 특정 클라이언트에게만 전송
+    // io.sockets.to(id).emit()
   });
 
-  // so.on("msg", function (data) {
-  //   console.log(data);
-
-  //   users[so.id] = data.uname;
-
-  //   io.sockets.emit("updateUsers", Object.values(users));
-
-  //   // public 모든 클라이언트에게 전송
-  //   io.sockets.emit("showMsg", data);
-
-  //   // broadcast 자신을 제외한 클라이언트들에게 전송
-  //   // so.broadcast.emit("showMsg", data);
-
-  //   // private 특정 클라이언트에게만 전송
-  //   // io.sockets.to(id).emit()
-  // });
-
   // 클라이언트가 연결을 끊을 때 참여자 업데이트
-  so.on("disconnect", function () {
+  so.on("disconnect", function (data) {
     const uname = users[so.id];
     delete users[so.id];
+
     io.sockets.emit("userOut", { uname: uname });
     io.sockets.emit("updateUsers", Object.values(users));
   });
-
-  // so.on("redUp", function (data) {
-  //   console.log(data);
-  //   io.sockets.emit("redUp", data);
-  // });
 });
--- onebite-react/section07/src/App.jsx
+++ onebite-react/section07/src/App.jsx
@@ -1,17 +1,31 @@
 import Controller from "./components/Controller";
 import Viewer from "./components/Viewer";
 import "./App.css";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
+import Even from "./components/Even";
 
 function App() {
   const [count, setCount] = useState(0);
+  const [input, setInput] = useState("");
 
+  const isMount = useRef(false);
+
+  // 1. 마운트 : 탄생
   useEffect(() => {
-    console.log(`count: ${count}`);
-  }, [count]);
-  // 의존성 배열
-  // dependency array
-  // deps
+    console.log("mount");
+  }, []);
+
+  // 2. 업데이트 : 변화, 리렌더링
+  useEffect(() => {
+    if (!isMount.current) {
+      isMount.current = true;
+      return;
+    }
+    console.log("update");
+  });
+
+  // 3. 언마운트 : 죽음
+  // Even.jsx
 
   const onClickButton = (value) => {
     setCount(count + value);
@@ -21,7 +35,16 @@ function App() {
     <div className="App">
       <h1>Simple Counter</h1>
       <section>
+        <input
+          value={input}
+          onChange={(e) => {
+            setInput(e.target.value);
+          }}
+        />
+      </section>
+      <section>
         <Viewer count={count} />
+        {count % 2 === 0 ? <Even /> : null}
       </section>
       <section>
         <Controller onClickButton={onClickButton} />
