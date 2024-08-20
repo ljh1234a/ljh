eeb1b2732112e0d92b65db8c71a47a6736b89491
ljh1234a <164473162+ljh1234a@users.noreply.github.com>
2024-08-20 12:24:01 +0900

    Add files via upload

---
+++ spring/oxQuiz/src/main/java/com/example/oxQuiz/OxQuizApplication.java
@@ -0,0 +1,12 @@
+package com.example.oxQuiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OxQuizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OxQuizApplication.class, args);
+	}
+}
---
+++ spring/oxQuiz/src/test/java/com/example/oxQuiz/OxQuizApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.oxQuiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OxQuizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
